4a652e6db0373fa3072394107a6da66e3bfa2b58
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-06-11 17:41:25 -0700

    page: added homepage with submission

---
+++ src/app/study/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function StudyConfigPage() {
+    return (
+        <div className="flex flex-col min-h-screen items-center bg-stone-900">
+            <h1 className="text-xl font-bold py-4 text-stone-300">Configure Study</h1>
+        </div>
+    )
+}
