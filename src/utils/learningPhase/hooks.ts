ad64170f5ff21c88c0045d343c8d26ed01c40b0a
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-07-21 16:17:03 -0700

    feat: added image retrieval and various bugfixes - Added image retrieval from backend - fixed minor issues with learning phase page and custom hooks - Added image fade in and fade out - removed unnecessary image list api call

--- src/utils/learningPhase/hooks.ts
+++ src/utils/learningPhase/hooks.ts
@@ -1,4 +1,70 @@
 import { useQuery } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { useCallback, useEffect, useMemo, useState } from "react";
+
+export function useLearningPhaseSequence(imageList: string[], config?: { display_duration: number; pause_duration: number; display_method: string }) {
+    const router = useRouter();
+    // routing variable just to make it cleaner
+    const goToNextPhase = useCallback(() => router.push("/study/waitPhase"), [router]);
+
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [pauseScreen, setPauseScreen] = useState(false);
+
+    const isManual = config?.pause_duration === 0;
+    const waitTimeMs = config?.pause_duration || 0;
+
+    // Shuffle image list if display method is random
+    // useMemo caches the random list instead of making a random array on each call,
+    // recalculates only when imageList changes.
+    const orderedImageList = useMemo(() => {
+        if (config?.display_method === "random") {
+            return [...imageList].sort(() => Math.random() - 0.5);
+        }
+        return imageList;
+    }, [imageList, config?.display_method]);
+
+    // Calculates if the client is on last image to change the button title from "Next" to "Continue to wait phase"
+    const isLastImage = currentIndex === orderedImageList?.length - 1;
+    const handleNext = () => {
+        if (currentIndex < orderedImageList.length - 1) {
+            setCurrentIndex(prev => prev + 1);
+        } else if (orderedImageList?.length > 1) {
+            goToNextPhase();
+        }
+    };
+
+    //handles both pause and display duration
+    useEffect(() => {
+        if (isManual) return;
+
+        let timer: NodeJS.Timeout;
+
+        if (pauseScreen) {
+            timer = setTimeout(() => setPauseScreen(false), waitTimeMs);
+        } else {
+            timer = setTimeout(() => {
+                if (currentIndex < orderedImageList?.length - 1) {
+                    setPauseScreen(true);
+                    setCurrentIndex(prev => prev + 1);
+                } else if (orderedImageList?.length > 1) {
+                    goToNextPhase();
+                }
+            }, config?.display_duration);
+        }
+
+        return () => clearTimeout(timer);
+    }, [currentIndex, pauseScreen, isManual, waitTimeMs, orderedImageList,config?.display_duration,goToNextPhase]);
+
+    return orderedImageList?.length === 0 || !config
+        ? null
+        : {
+            currentImage: orderedImageList[currentIndex],
+            pauseScreen,
+            isManual,
+            isLastImage,
+            handleNext
+        };
+}
 
 export function useLearningPhaseConfig(studyID: string) {
     return useQuery({
@@ -6,24 +72,7 @@ export function useLearningPhaseConfig(studyID: string) {
         queryFn: async () => {
             const res = await fetch(`/api/study/learning_phase/${studyID}`);
             if (!res.ok) throw new Error("Failed to fetch learning phase config");
-            return res.json(); // { display_duration, pause_duration, display_method }
-        },
-        enabled: !!studyID,
-    });
-}
-
-export function useLearningImageList(studyID: string) {
-    return useQuery({
-        queryKey: ["learningImageList", studyID],
-        queryFn: async () => {
-            // TODO: Change the fetch endpoint below once the a definite endpoint is defined
-            const res = await fetch(`/api/study/learning_phase_images/${studyID}`); //Assuming the endpoint for now
-            if (!res.ok) throw new Error("Failed to fetch image list");
-            const text = await res.text(); // text instead of json because we are getting the results as Stream of downloaded CSV
-            return text
-                .split("\n")
-                .map(line => line.trim()) // Remove extra spaces/tabs from each line
-                .filter(Boolean);  // Keep only truthy lines (removes "", null, undefined, fasle ...)
+            return res.json(); // { display_duration, pause_duration, display_method, image_urls }
         },
         enabled: !!studyID,
     });
