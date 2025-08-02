410610ed6a7f1f2fc24487669f1c26fc348f0d83
Beth Ann <124757398+bethann-hoot@users.noreply.github.com>
2025-08-01 19:33:26 -0700

    feat: reusable radio

--- src/app/study/experimentPhase/page.tsx
+++ src/app/study/experimentPhase/page.tsx
@@ -6,17 +6,31 @@ import ImageDisplayComponent from "@/components/UserView/ImageDisplay";
 
 export default function ExperimentPhasePage() {
     const studyID = useStudyID();
+<<<<<<< HEAD
     //const { data: imageList, isLoading: loadingImages } = useExperimentImageList(studyID);
     const { data: config, isLoading: loadingConfig } = useExperimentPhaseConfig(studyID);
 
     if (loadingConfig) return <div className="text-white">Loading...</div>;
+=======
+    const { data: config, isLoading: loadingConfig } = useExperimentPhaseConfig(studyID);
+    if (loadingConfig) return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-stone-900">
+            <span className="loader"></span>
+        </div>);
+>>>>>>> ee279f6 (feat: reusable radio)
     if (!config) return <div className="text-red-500">Failed to load data.</div>;
 
     return (
         <ImageDisplayComponent
             config={config}
+<<<<<<< HEAD
             nextPhaseName={"Conclusion"}
             nextPhaseRoute={"/study/conclusionPhase"}
         />
     );
 }
+=======
+        />
+    );
+}
+>>>>>>> ee279f6 (feat: reusable radio)
