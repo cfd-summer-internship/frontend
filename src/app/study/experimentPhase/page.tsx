d132f3fd6a342ff415a1039dd653bc984fb0dd23
Mia Fraser <mia.jaylen@proton.me>
2025-08-18 22:20:14 +0000

    feat: response time and image ids - Reworked backend to include image ids in api response - Updated image display component and usePhaseSequence hook to include image ids and urls - added reaction time via performance.now() on image load

--- src/app/study/experimentPhase/page.tsx
+++ src/app/study/experimentPhase/page.tsx
@@ -6,12 +6,22 @@ import ImageDisplayComponent from "@/components/UserView/ImageDisplay";
 
 export default function ExperimentPhasePage() {
     const studyID = useStudyID();
+<<<<<<< HEAD
     const { data: config, isLoading: loadingConfig } = useExperimentPhaseConfig(studyID);
     if (loadingConfig) return (
         <div className="flex flex-col items-center justify-center min-h-screen bg-stone-900">
             <span className="loader"></span>
         </div>);
     if (!config) return <div className="text-red-500">Failed to load data.</div>;
+=======
+
+    const { data: config, isLoading: loadingConfig } = useExperimentPhaseConfig(studyID);
+
+    if (loadingConfig || !config) return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-stone-900 text-white px-4">
+            <span className="loader"></span>
+        </div>)
+>>>>>>> 6a19e08 (feat: response time and image ids)
 
     return (
         <ImageDisplayComponent
