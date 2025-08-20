5508ba6c4407657b49f1a4dd96bb6e05995116aa
Mia Fraser <mia.jaylen@proton.me>
2025-08-20 00:43:30 +0000

    fix: updated endpoint

--- src/utils/experimentPhase/index.ts
+++ src/utils/experimentPhase/index.ts
@@ -1,10 +1,17 @@
 import { StudyResponseList } from "@/schemas/studyResponseSchemas";
 
-export const submitAnswers = (async (studyID:string, subjectID:string, answers: StudyResponseList) => {
-    const res = await fetch(`/api/results/responses/${studyID}?subject_id=${subjectID}`, {
+export const submitAnswers = (async (configID:string, subjectID:string, answers: StudyResponseList) => {
+    const body={
+        "identity":{
+            "config_id":configID,
+            "subject_id":subjectID
+        },
+        "responses":answers
+    }
+    const res = await fetch(`/api/results/responses/`, {
         method: "POST",
         headers:{ "Content-Type": "application/json",},
-        body: JSON.stringify(answers)
+        body: JSON.stringify(body)
     });
 
     if (!res.ok) {
