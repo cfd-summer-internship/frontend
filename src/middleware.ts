31697f5955aaa69e644d55b464aa9733786aacb9
Plixelated <76276169+Plixelated@users.noreply.github.com>
2025-10-02 23:34:51 -0700

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { MiddlewareConfig, NextRequest, NextResponse } from "next/server";
 export function middleware(req: NextRequest) {
     if(req.nextUrl.pathname.startsWith("/api")) {
         return NextResponse.rewrite(new URL(
-            `${process.env.API_URL}${req.nextUrl.pathname.replace("/api", "")}${req.nextUrl.search}`
+            `${process.env.NEXT_PUBLIC_API_URL}${req.nextUrl.pathname.replace("/api", "")}${req.nextUrl.search}`
         ), {request: req})
     }
 }
